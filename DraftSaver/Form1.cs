279a2a61cc8dea1ffd94b27587d9b6935cc9c33f
DomePB <dome2000x@gmail.com>
2023-11-14 20:34:52 +0100

    Only once every Champion Check + pick order

--- DraftSaver/Form1.cs
+++ DraftSaver/Form1.cs
@@ -5,6 +5,7 @@ namespace DraftSaver
         private String[] ChampionsArr = { "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nocturne", "Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra" };
         private int pickIndex = 0;
         private Label[] Picks;
+        private String[] Picked = new string[10];
         public Form1()
         {
             InitializeComponent();
@@ -22,8 +23,12 @@ namespace DraftSaver
             if (Champions.SelectedItem != null && pickIndex<10)
             {
                 string? test = Champions.SelectedItem as string;
-                Picks[pickIndex].Text = test;
-                pickIndex++;
+                if (!Picked.Contains(test))
+                {
+                    Picked[pickIndex] = test;
+                    Picks[pickIndex].Text = test;
+                    pickIndex++;
+                }
             }
 
           
@@ -33,8 +38,17 @@ namespace DraftSaver
         private void InitializeLabels() {
             Picks = new Label[]
             {
-                B1Pick, B2Pick, B3Pick, B4Pick, B5Pick,
-                R1Pick  , R2Pick, R3Pick,   R4Pick,   R5Pick,
+               B1Pick,
+               R1Pick,
+               R2Pick,
+               B2Pick,
+               B3Pick,
+               R3Pick,
+               R4Pick,
+               B4Pick,
+               B5Pick,
+               R5Pick,
+
             };
         }
     }
