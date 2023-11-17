28606a679c3582bc098b81c04615432190b2df34
DomePB <dome2000x@gmail.com>
2023-11-17 17:40:14 +0100

    Fixed Saving pick order

--- DraftSaver/DatabaseConnection.cs
+++ DraftSaver/DatabaseConnection.cs
@@ -42,7 +42,7 @@ namespace DraftSaver
         public void Save(string[] picks) 
         {
             Open();
-            SqlCommand matchCommand = new SqlCommand($"INSERT INTO SavedDrafts(B1Pick,B2Pick,B3Pick,B4Pick,B5Pick,R1Pick,R2Pick,R3Pick,R4Pick,R5Pick) VALUES ('{picks[0]}','{picks[1]}','{picks[2]}','{picks[3]}','{picks[4]}','{picks[5]}','{picks[6]}','{picks[7]}','{picks[8]}','{picks[9]}')",cnn);
+            SqlCommand matchCommand = new SqlCommand($"INSERT INTO SavedDrafts(B1Pick,B2Pick,B3Pick,B4Pick,B5Pick,R1Pick,R2Pick,R3Pick,R4Pick,R5Pick) VALUES ('{picks[0]}','{picks[3]}','{picks[4]}','{picks[7]}','{picks[8]}','{picks[1]}','{picks[2]}','{picks[5]}','{picks[6]}','{picks[9]}')",cnn);
             matchCommand.ExecuteNonQuery();
             Close();
         }
